8814a851f2409b2fdfb5b4850cebea9a9dd6831a
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-18 14:08:17 -0300

    Adiciona resolução de Exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Box_Exercicie;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("++------------------++");
+        Console.WriteLine("|| Brenno Prettyman ||");
+        Console.WriteLine("++------------------++");
+    }
+}
